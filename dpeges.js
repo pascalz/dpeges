65d42f601404260811f6f6a710d17b7325c21147
Rémy <remy@sleede.com>
2016-10-18 17:55:36 +0200

    add missing viewBox attribute, needed to enable responsive resizing

--- dpeges.js
+++ dpeges.js
@@ -177,6 +177,7 @@ var DpeGes = function () {
         svg.setAttribute("width", self.width);
         svg.setAttribute("height", self.height);
         svg.setAttribute("version", "1.1");
+        svg.setAttribute("viewBox", "0 0 " + self.width + " " + self.height);
         svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
 
         var desc = document.createElementNS(svgNS, "desc");
